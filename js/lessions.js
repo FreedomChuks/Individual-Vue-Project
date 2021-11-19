2ea586be7bdd701db5d1e0c7c1f97978baedd26d
freedom <freedom.chuks7@gmail.com>
2021-11-19 01:10:09 +0100

    created lession object

---
+++ js/lessions.js
@@ -0,0 +1,92 @@
+let lessons = [
+	{
+        id: 1,
+        subject: "Android Development",
+        location: "London",
+        price : 500,
+        image: "images/androidDev.png",
+        availablespace: 5,
+        rating: 'fa fa-star'
+    },
+    {
+        id: 2,
+        subject: "IOS Development",
+        location: "Hendon",
+        price : 750,
+        image: "images/appleDev.png",
+        availablespace: 5,
+        rating: 'fa fa-star'
+    },
+    {
+        id: 3,
+        subject: "Angular",
+        location: "Camden",
+        price : 450,
+        image: "images/angularDev.png",
+        availablespace: 5,
+        rating: 'fa fa-star'  
+    },
+    {
+        id: 4,
+        subject: "ReactJS",
+        location: "Canning Town",
+        price : 600,
+        image: "images/reactJS.png",
+        availablespace: 5,
+        rating: 'fa fa-star'    
+    },
+    {
+        id: 5,
+        subject: "Learn VueJS",
+        location: "Westfield",
+        price : 550,
+        image: "images/vueJS.png",
+        availablespace: 5,
+        rating: 'fa fa-star'    
+    },
+    {
+        id: 6,
+        subject: "Learn Kotlin",
+        location: "Stratford",
+        price : 250,
+        image: "images/kotlin.png",
+        availablespace: 5,
+        rating: 'fa fa-star'    
+    },
+    {
+        id: 7,
+        subject: "Go (Server-Side Dev)",
+        location: "Westfield",
+        price : 450,
+        image: "images/go.png",
+        availablespace: 5,
+        rating: 'fa fa-star'   
+    },
+    {
+        id: 8,
+        subject: "Understanding UI/UI",
+        location: "Chelsea",
+        price : 950,
+        image: "images/adobeXD.png",
+        availablespace: 5,
+        rating: 'fa fa-star' 
+    },
+    {
+        id: 9,
+        subject: "Backend Development",
+        location: "Chelsea",
+        price : 950,
+        image: "images/web.jpg",
+        availablespace: 5,
+        rating: 'fa fa-star'
+    },
+    {
+        id: 10,
+        subject: "Learn Microsoft c#",
+        location: "Old Traford",
+        price : 450,
+        image: "images/csharp.png",
+        availablespace: 5,
+        rating: 'fa fa-star'      
+    },
+]
